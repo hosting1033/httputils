479f013c2653cfb6ecf12fe486ba86b125990e82
Alex Ry <alex1033@gmail.com>
2018-02-05 12:34:53 +0100

    simple proxy test

--- src/main/java/com/arit/utils/controller/BasicController.java
+++ src/main/java/com/arit/utils/controller/BasicController.java
@@ -1,14 +1,47 @@
 package com.arit.utils.controller;
 
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicController {
+	
+	public static String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+
+
+	private CloseableHttpClient httpClient = HttpClients.custom().setUserAgent(userAgent).build();
 
 	@RequestMapping("/test")
     public String index() {
-        return "From Spring Boot!";
+		String result = "error proxy";
+ try {
+			 
+			 HttpGet httpget = new HttpGet(new URI("http://www.myipaddress.com/show-my-ip-address/"));
+			
+             CloseableHttpResponse response = httpClient.execute(httpget);
+             try {
+                                
+                 HttpEntity entity = response.getEntity();
+                 if (entity != null) {
+                	
+                       result = EntityUtils.toString(entity);
+                 }
+             } finally {
+                 response.close();
+             }
+         } catch (Exception e) {
+             return(e.getMessage());
+         }
+		return result;
+       
     }
 	
 }
