d63ceed519d571932a5d7ce2dd2f0e22bdf6fcf2
Alex Ry <alex1033@gmail.com>
2018-02-05 19:42:17 +0100

    formating

--- src/main/java/com/arit/utils/controller/BasicController.java
+++ src/main/java/com/arit/utils/controller/BasicController.java
@@ -13,35 +13,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicController {
-	
-	public static String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
 
+	public static String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
 
 	private CloseableHttpClient httpClient = HttpClients.custom().setUserAgent(userAgent).build();
 
 	@RequestMapping("/test")
-    public String index() {
+	public String index() {
 		String result = "error proxy";
- try {
-			 
-			 HttpGet httpget = new HttpGet(new URI("http://www.myipaddress.com/show-my-ip-address/"));
-			
-             CloseableHttpResponse response = httpClient.execute(httpget);
-             try {
-                                
-                 HttpEntity entity = response.getEntity();
-                 if (entity != null) {
-                	
-                       result = EntityUtils.toString(entity);
-                 }
-             } finally {
-                 response.close();
-             }
-         } catch (Exception e) {
-             return(e.getMessage());
-         }
+		try {
+
+			HttpGet httpget = new HttpGet(new URI("http://www.myipaddress.com/show-my-ip-address/"));
+
+			CloseableHttpResponse response = httpClient.execute(httpget);
+			try {
+
+				HttpEntity entity = response.getEntity();
+				if (entity != null) {
+
+					result = EntityUtils.toString(entity);
+				}
+			} finally {
+				response.close();
+			}
+		} catch (Exception e) {
+			return (e.getMessage());
+		}
 		return result;
-       
-    }
-	
+
+	}
+
 }
